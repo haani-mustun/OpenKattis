e9ff8f2b7681c69f3e5a2403f473acc54666ea76
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 18:43:30 +0100

    Committing R2
    Although line 10 can be combined with line 11, I thought I would split it just so that its a little easier to read.
    
    We want to take the input from the user, and then find the difference between the second number and the first number. We then add that difference to the second number to find R2.

---
+++ c#/R2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace R2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int difference = input[1] - input[0];
+            Console.WriteLine(input[1] + difference);
+        }
+    }
+}
