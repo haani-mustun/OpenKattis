02e1e67e1f94c9a513c8747c2bc2583d87ed5b72
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 16:43:22 +0100

    Committing TakeTwoStones

---
+++ c#/TakeTwoStones.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TakeTwoStones
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine((int.Parse(Console.ReadLine()) % 2 == 0) ? "Bob" : "Alice");
+        }
+    }
+}
