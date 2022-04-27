f32c8343d242360476652185416ba5204ee1744e
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 18:38:20 +0100

    Committing SolvingForCarrots
    A few red herrings in the question, we essentially just want to read in the first line of input and then return the second value in that first line.

---
+++ c#/SolvingForCarrots.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SolvingForCarrots
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            Console.WriteLine(input[1]);
+        }
+    }
+}
