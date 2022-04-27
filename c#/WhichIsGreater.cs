7c8cf91ce00921a746bf6f3322b343ab76d15a27
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 16:09:06 +0100

    Committing WhichIsGreater
    This is the Which is Greater solution in c#. Was quite simply, so only needs 1 initial commit

---
+++ c#/WhichIsGreater.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace WhichIsGreater
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            if (inputs[0] > inputs[1])
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
