8938b4aad82fb29aa15d22b0192a5d0a4c8a3717
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 22:58:06 +0100

    Committing Nasty Hacks
    This is simply reading in the data values, and then comparing the subtraction of the 3rd and 2nd values to the 1st value

---
+++ c#/NastyHacks.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace NastyHacks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < cases; i++)
+            {
+                int[] revenueValues = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+                if (revenueValues[1] - revenueValues[2] > revenueValues[0])
+                {
+                    Console.WriteLine("advertise");
+                }
+                else if (revenueValues[1] - revenueValues[2] == revenueValues[0])
+                {
+                    Console.WriteLine("does not matter");
+                }
+                else
+                {
+                    Console.WriteLine("do not advertise");
+                }
+            }
+        }
+    }
+}
