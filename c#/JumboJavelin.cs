9fb5f5e48adddec96482e5f697a1a9fac18b2dbb
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 23:45:54 +0100

    Committing Jumbo Javelin
    We simply just want to take the first input and use that as our condition in our for loop
    
    Then we first loop to get all the other javelin lengths, and then total all of those values.
    
    But remember, the javelin length uses a 1cm after the 1st javelin, so we need to take that into account.

---
+++ c#/JumboJavelin.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace JumboJavelin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOfJavelins = int.Parse(Console.ReadLine());
+            int[] javelinLengths = new int[numOfJavelins];
+
+            for (int i = 0; i < numOfJavelins; i++)
+            {
+                javelinLengths[i] = int.Parse(Console.ReadLine());
+            }
+
+            int totalLength = 0;
+
+            for (int i = 0; i < javelinLengths.Length; i++)
+            {
+                totalLength += javelinLengths[i];
+            }
+
+            Console.WriteLine(totalLength - numOfJavelins + 1);
+        }
+    }
+}
