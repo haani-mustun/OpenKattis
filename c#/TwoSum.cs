556034b3a22b7dcdb35259523188446356d3e4e7
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 16:12:17 +0100

    Committing TwoSum
    Another simple solution to the TwoSum problem, only needs 1 commit.

---
+++ c#/TwoSum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TwoSum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            Console.WriteLine(input[0] + input[1]);
+        }
+    }
+}
