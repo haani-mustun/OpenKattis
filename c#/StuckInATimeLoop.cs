e272fff171aaa66c73f01aa94b0801375ee01b10
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 18:33:10 +0100

    Committing StuckInATimeLoop
    Take in the input, and then using a for loop to print the output

---
+++ c#/StuckInATimeLoop.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace StuckInATimeLoop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int input = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < input + 1; i++)
+            {
+                Console.WriteLine(i + " Abracadabra");
+            }
+        }
+    }
+}
