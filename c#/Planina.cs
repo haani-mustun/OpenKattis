10fbfad2db577859ecbc9410e9861f081c598f67
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 19:53:06 +0100

    Committing Planina
    This one was a little bit more complicated and took a little while longer, however when you break down the question it makes it a lot simplier.
    
    If you can figure out the pattern in which the number of dots increase, you can then easily work out the number of dots at a given interation.
    
    The pattern is this:
    
    The number of dots is = to the starting number of dots square rooted, and increased iteratively by a rate of increase. The rate of increase starts at 1, and is doubled every iteration.

---
+++ c#/Planina.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Planina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int interations = int.Parse(Console.ReadLine());
+
+            int startSqrted = 2;
+            int rateOfIncrease = 1;
+
+            for (int i = 0; i < interations; i++)
+            {
+                startSqrted += rateOfIncrease;
+                rateOfIncrease *= 2;
+            }
+
+            int total = startSqrted * startSqrted;
+
+            Console.WriteLine(total);
+        }
+    }
+}
