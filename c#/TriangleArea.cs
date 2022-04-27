2f1c16b99744e12d16fd045667d2c9903dcb18b2
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 16:18:36 +0100

    Committing Triangle Area
    Committing the completed solution for the Triangle Area problem

---
+++ c#/TriangleArea.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float[] inputs = Array.ConvertAll(Console.ReadLine().Split(' '), s => float.Parse(s));
+
+            Console.WriteLine((inputs[0] * inputs[1]) / 2);
+        }
+    }
+}
