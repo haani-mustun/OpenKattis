0ff01fdd88391a9ef4337a7413f3c270ccfffda2
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 19:37:39 +0100

    Committing QuadrantSelection
    Although there are many, equally just as good ways to completing this problem, i decided to utilise switch statements to identify which quadrant the coordinates lie in

---
+++ c#/QuadrantSelection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace QuadrantSelection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool PositiveX = int.Parse(Console.ReadLine()) < 0 ? false : true;
+            bool PositiveY = int.Parse(Console.ReadLine()) < 0 ? false : true;
+
+            switch (PositiveX)
+            {
+                case true:
+                    switch (PositiveY)
+                    {
+                        case true:
+                            Console.WriteLine("1");
+                            break;
+                        case false:
+                            Console.WriteLine("4");
+                            break;
+                    }
+                    break;
+                case false:
+                    switch (PositiveY)
+                    {
+                        case true:
+                            Console.WriteLine("2");
+                            break;
+                        case false:
+                            Console.WriteLine("3");
+                            break;
+                    }
+                    break;
+            }
+        }
+    }
+}
