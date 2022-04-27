ae5ec449875d976ef9f6088beaa67b0ca0e11120
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 16:29:36 +0100

    Committing Tarifa Solution
    Committing the Tarifa Solution.
    Just had to use two loops, one that takes in the input, and one that calculates the output

---
+++ c#/Tarifa.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Tarifa
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int StandardRateX = int.Parse(Console.ReadLine());
+            int NumberOfMonths = int.Parse(Console.ReadLine());
+            int[] NumOfMegabytes = new int[NumberOfMonths];
+            for (int i = 0; i < NumberOfMonths; i++)
+            {
+                NumOfMegabytes[i] = int.Parse(Console.ReadLine());
+            }
+
+            int PassOver = 0;
+            foreach(int i in NumOfMegabytes)
+            {
+                PassOver += (StandardRateX - i);
+            }
+            PassOver += StandardRateX;
+            Console.WriteLine(PassOver);
+        }
+    }
+}
