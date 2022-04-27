d57a4bd0f35c450767a83322b9f9a3218a0855ed
haanimustun <haanimustun@hotmail.co.uk>
2022-04-27 18:51:02 +0100

    Committing QALY
    This one is a little different since we are dealing with floats instead, however we still want to take the first line of input to instantiate our for loop
    
    Now we want to keep repeating the loop and incrementing the values in cumulativeQALY until we have finished our loop.

---
+++ c#/QALY.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace QALY
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumOfPeriods = int.Parse(Console.ReadLine());
+            float cumulativeQALY = 0.0f;
+
+            for (int i = 0; i < NumOfPeriods; i++)
+            {
+                float[] values = Array.ConvertAll(Console.ReadLine().Split(' '), s => float.Parse(s));
+                cumulativeQALY += values[0] * values[1];
+            }
+
+            Console.WriteLine(cumulativeQALY);
+        }
+    }
+}
