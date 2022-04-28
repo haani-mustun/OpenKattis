55e5d53c23c0ddb7db60dab61631ca450cc38604
haanimustun <haanimustun@hotmail.co.uk>
2022-04-28 14:33:16 +0100

    Committing IsItHalloween
    Since no arithmetic is needed for this problem, we don't need to convert the day into an integer, we can simply keep it as a string.
    
    We use a switch statement initially to determine whether or not the month is OCT or DEC, and then we use an if statement to see if the day is 31 for OCT or 25 for DEC.

---
+++ c#/IsItHalloween.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace IsItHalloween
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] MonthAndDay = Console.ReadLine().Split(' ');
+            switch (MonthAndDay[0])
+            {
+                case "OCT":
+                    if (MonthAndDay[1] == "31")
+                    {
+                        Console.WriteLine("yup");
+                    }
+                    else
+                    {
+                        Console.WriteLine("nope");
+                    }
+                    break;
+                case "DEC":
+                    if (MonthAndDay[1] == "25")
+                    {
+                        Console.WriteLine("yup");
+                    }
+                    else
+                    {
+                        Console.WriteLine("nope");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("nope");
+                    break;
+            }
+        }
+    }
+}
