3e4a57bbb13ebb0f39f43aa0d6b2a9379f8e07f0
haanimustun <haanimustun@hotmail.co.uk>
2022-04-28 14:26:11 +0100

    Committing JackOLanternJuxtaposition
    This is a simple one, however I wanted to test out how the array.Aggregate function works from the System.Linq library.
    
    The Aggregate function, from what i can gather, total's up the values in an array whether that is summative or multiplicative

---
+++ c#/JackOLanternJuxtaposition.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace JackOLanternJuxtaposition
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] designs = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            Console.WriteLine(designs.Aggregate((total, next) => total * next));
+        }
+    }
+}
