9e080a9194e1695650cbba9416beff4a5960cc17
haanimustun <haanimustun@hotmail.co.uk>
2022-04-28 22:04:48 +0100

    Committing HeartRate
    I got a bit confusing when calculating the min and max bpm, but it turns out that its just the variance value + or - the bpm we calculated using the formula

---
+++ c#/HeartRate.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace HeartRate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int cases = int.Parse(Console.ReadLine());
+            for (int i = 0; i < cases; i++)
+            {
+                float[] BeatsAndTime = Array.ConvertAll(Console.ReadLine().Split(' '), s => float.Parse(s));
+                float bpm = (60 * BeatsAndTime[0]) / BeatsAndTime[1];
+                float minbpm = bpm - (60 / BeatsAndTime[1]);
+                float maxbpm = bpm + (60 / BeatsAndTime[1]);
+
+                Console.WriteLine(minbpm + " " + bpm + " " + maxbpm);
+            }
+        }
+    }
+}
