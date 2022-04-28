d4bc815f88735bd2440846d2c293f82ad416e3a6
haanimustun <haanimustun@hotmail.co.uk>
2022-04-28 14:46:25 +0100

    Committing HissingMicrophone
    Remember that when using a for loop to check values that are i+1 in an array, we will eventually run into a index out of array bounds error, and therefore we need to make sure that i is always less than the length - 1.

---
+++ c#/HissingMicrophone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace HissingMicrophone
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int i;  
+
+            for (i = 0; i < input.Length; i++)
+            {
+                if (input[i] == 's' && i < input.Length - 1)
+                {
+                    if (input[i + 1] == 's')
+                    {
+                        Console.WriteLine("hiss");
+                        break;
+                    }
+                    else
+                    {
+                        i += 1;
+                    }
+                }
+            }
+
+            if (i == input.Length)
+            {
+                Console.WriteLine("no hiss");
+            }
+        }
+    }
+}
