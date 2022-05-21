3efa9ad2e59d877c7e1efc495eff2ff364d3d945
haanimustun <haanimustun@hotmail.co.uk>
2022-05-21 22:35:59 +0100

    Committing GrassSeedInc
    First we want to read in our seed cost and how many lawns we're dealing with. Then we can use a for loop to calculate the area of each lawn, and then increment our total cost by multiplying the seed cost with the lawn area for each lawn

---
+++ c#/GrassSeedInc.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Grass_Seed_Inc
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double seedCost = double.Parse(Console.ReadLine());
+            int numberOfLawns = int.Parse(Console.ReadLine());
+            double totalCost = 0.0f;
+            for (int i = 0; i < numberOfLawns; i++)
+            {
+                double[] lawnDimensions = Array.ConvertAll(Console.ReadLine().Split(' '), s => double.Parse(s));
+                double lawnSize = lawnDimensions[0] * lawnDimensions[1];
+                totalCost += seedCost * lawnSize;
+            }
+            Console.WriteLine(totalCost);
+        }
+    }
+}
