0e632884dcf758f14a4fc83b93b6e5da1edc707a
haanimustun <haanimustun@hotmail.co.uk>
2022-05-21 22:10:36 +0100

    Completed GCVWR
    First we want to read in our values, then we simply follow how the problem describes calculating the maximum weight of the trailer

---
+++ c#/GCVWR.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GCVWR
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] WeightAndItems = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+            int[] ItemWeights = Array.ConvertAll(Console.ReadLine().Split(' '), s => int.Parse(s));
+
+            float GminusT = (WeightAndItems[0] - WeightAndItems[1]) * 0.9f;
+            int CombinedItemWeight = 0;
+            for (int i = 0; i < WeightAndItems[2]; i++)
+            {
+                CombinedItemWeight += ItemWeights[i];
+            }
+            Console.WriteLine(GminusT - CombinedItemWeight);
+        }
+    }
+}
