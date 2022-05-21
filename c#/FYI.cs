d3a4d972f51f41b2e5834103704055d8af8b3075
haanimustun <haanimustun@hotmail.co.uk>
2022-05-21 23:03:08 +0100

    Committing FYI
    We just want to take the first 3 digits from the input, and then check if its 555

---
+++ c#/FYI.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace FYI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string areaCode = Console.ReadLine().Substring(0, 3);
+            if (areaCode == "555")
+            {
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
