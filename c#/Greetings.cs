4fed85e3e206beec453b79f6ea503b58b6f19e7a
haanimustun <haanimustun@hotmail.co.uk>
2022-05-21 22:27:40 +0100

    Committing Greetings
    First we want to count how many Es are in the "hey" that is received, and we can do that by comparing the current length of the received string and subtracting the length of the string without any of the Es, then we run a for loop to adding twice as many Es between the H and the Y

---
+++ c#/Greetings.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Greetings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string receivingHey = Console.ReadLine();
+            int numberOfEs = (receivingHey.Length - receivingHey.Replace("e", string.Empty).Length) * 2;
+            string sendingHey = "h";
+            for (int i = 0; i < numberOfEs; i++)
+            {
+                sendingHey += "e";
+            }
+            Console.WriteLine(sendingHey + "y");
+        }
+    }
+}
