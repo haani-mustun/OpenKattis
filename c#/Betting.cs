0a584739803b252cc54a0a0272141a267d4ca4a4
haanimustun <haanimustun@hotmail.co.uk>
2022-05-21 22:22:37 +0100

    Committing Betting
    The trick to this one is just understanding how ratios work. In order to calculate the 2nd half of the ratio, you want to ask the question "How many times does x go into 1", with x being 80 in the example that we provided in the question.

---
+++ c#/Betting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Betting
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float Percentage = float.Parse(Console.ReadLine()) / 100;
+            Console.WriteLine(1 / Percentage);
+            Console.WriteLine(1 / (1 - Percentage));
+        }
+    }
+}
